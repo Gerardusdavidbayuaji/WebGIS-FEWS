d9d58599b6a765f5b02fb3a5311745551ad0137f
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2024-07-11 15:54:37 +0700

    feat: update dashboard admin

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Dashboard from "./pages/dasboard";
-import Basemap from "./__test__/basemap";
+import DashboardAdmin from "./pages/admin/dashboard-admin";
+import ManagementAdmin from "./pages/admin/management-admin";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Dashboard />
+    <ManagementAdmin />
   </React.StrictMode>
 );
